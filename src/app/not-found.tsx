c4f4926fc3392a824f87b296533f0895d393017f
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2024-08-04 19:09:44 +0400

    app write

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export default function notFound() {
@@ -9,7 +8,7 @@ export default function notFound() {
             <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
             <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Something's missing.</p>
             <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
-            <Button>Back to Homepage</Button>
+            
         </div>   
     </div>
 </section>
---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,6 @@
+const conf = {
+    appwriteUrl: String('https://cloud.appwrite.io/v1'),
+    appwriteProjectId: String(process.env.APPWRITE_PROJECT_ID),
+}
+
+export default conf
