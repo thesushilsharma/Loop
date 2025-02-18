a1280c444bd503dbbf7206ed80a00c9388d306b1
Sushil <ss362@uowmail.edu.au>
2025-02-19 02:20:45 +0400

    adding changes or pg table

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,72 +1,129 @@
-import { pgTable, serial, varchar, boolean, timestamp, integer, text, uuid } from "drizzle-orm/pg-core";
-import { relations } from "drizzle-orm"
+import {
+    pgTable,
+    serial,
+    varchar,
+    boolean,
+    timestamp,
+    integer,
+    text,
+    uuid,
+    index,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 
+const createdAt = timestamp("created_at").notNull().defaultNow();
+const updatedAt = timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date());
+
 // Users table
-export const users = pgTable("users", {
-    id: uuid("id").primaryKey().defaultRandom(),
-    authId: integer("auth_Id").notNull(),
-    name: text("name").notNull(),
-    emailId: text("email").notNull().unique(), //passwordless login email otp
-})
+export const users = pgTable(
+    "users",
+    {
+        id: uuid("id").primaryKey().defaultRandom(),
+        authId: text("auth_Id").notNull(),
+        name: text("name").notNull(),
+        emailId: text("email").notNull().unique(), //passwordless login email otp
+        createdAt,
+    },
+    (table) => [
+        index("auth_user_id_idx").on(table.authId),
+        index("email_idx").on(table.emailId),
+    ]
+);
 
 // Universities table
-export const universities = pgTable('universities', {
-    universityId: serial('university_id').primaryKey(),
-    title: varchar('title', { length: 100 }).notNull(),
-    description: text('description'),
-    country: varchar('country', { length: 100 }).notNull(),
-    region: varchar('region', { length: 100 }).notNull(),
-    address: text('address').notNull(),
-    maps: varchar('maps_url', { length: 255 }),
-    imageUrl: varchar('image_url', { length: 255 }),
-    websiteLink: varchar('website_url', { length: 255 }).notNull(),
-    linkedinLink: varchar('linkedin_url', { length: 255 }),
-    rating: integer('rating').notNull().notNull(),
-    createdAt: timestamp('created_at').defaultNow(),
+export const universities = pgTable("universities", {
+    universityId: serial("university_id").primaryKey(),
+    title: varchar("title", { length: 100 }).notNull(),
+    description: text("description"),
+    country: varchar("country", { length: 100 }).notNull(),
+    region: varchar("region", { length: 100 }).notNull(),
+    address: text("address").notNull(),
+    maps: varchar("maps_url", { length: 255 }),
+    imageUrl: varchar("image_url", { length: 255 }),
+    websiteLink: varchar("website_url", { length: 255 }).notNull(),
+    linkedinLink: varchar("linkedin_url", { length: 255 }),
+    rating: integer("rating").notNull().notNull(),
+    createdAt,
 });
 
 // Reviews table
-export const reviews = pgTable('reviews', {
-    reviewId: serial('review_id').primaryKey(),
-    authId: integer('auth_Id').notNull(),
-    universityId: serial('university_id').notNull().references(() => universities.universityId, {
-        onDelete: 'cascade'
-    }),
-    rating: integer('rating').notNull().notNull(),
-    content: text('content').notNull(),
-    votes: integer('votes').notNull().default(0),
-    createdAt: timestamp('created_at').defaultNow(),
-    updatedAt: timestamp('updated_at').defaultNow(),
-});
+export const reviews = pgTable(
+    "reviews",
+    {
+        reviewId: serial("review_id").primaryKey(),
+        authId: text("auth_Id")
+            .notNull()
+            .references(() => users.authId, {
+                onDelete: "cascade",
+            }),
+        universityId: serial("university_id")
+            .notNull()
+            .references(() => universities.universityId, {
+                onDelete: "cascade",
+            }),
+        rating: integer("rating").notNull().notNull(),
+        content: text("content").notNull(),
+        votes: integer("votes").notNull().default(0),
+        createdAt: timestamp("created_at").defaultNow(),
+        updatedAt: timestamp("updated_at").defaultNow(),
+    },
+    (table) => [index("user_review_idx").on(table.authId, table.universityId)]
+);
 
 // Posts table
-export const posts = pgTable('posts', {
-    postId: serial('post_id').primaryKey(),
-    authId: integer('auth_Id').notNull(),
-    universityId: serial('university_id').notNull().references(() => universities.universityId, {
-        onDelete: 'cascade'
-    }),
-    title: varchar('title', { length: 200 }).notNull(),
-    content: text('content').notNull(),
-    isUpVote: boolean('is_up_vote').notNull().default(false),
-    createdAt: timestamp('created_at').defaultNow(),
-    updatedAt: timestamp('updated_at').defaultNow(),
-});
+export const posts = pgTable(
+    "posts",
+    {
+        postId: serial("post_id").primaryKey(),
+        authId: text("auth_Id")
+            .notNull()
+            .references(() => users.authId, {
+                onDelete: "cascade",
+            }),
+        universityId: serial("university_id")
+            .notNull()
+            .references(() => universities.universityId, {
+                onDelete: "cascade",
+            }),
+        title: varchar("title", { length: 200 }).notNull(),
+        content: text("content").notNull(),
+        isUpVote: boolean("is_up_vote").notNull().default(false),
+        createdAt,
+        updatedAt,
+    },
+    (table) => [
+        index("title_idx").on(table.title),
+        index("user_post_idx").on(table.authId),
+    ]
+);
 
 // Comments table
-export const comments = pgTable('comments', {
-    commentId: serial('comment_id').primaryKey(),
-    authId: integer('auth_Id').notNull(),
-    postId: serial('post_id').notNull().references(() => posts.postId, {
-        onDelete: 'cascade'
-    }),
-    content: text('content').notNull(),
-    votes: integer('votes').notNull().default(0),
-    createdAt: timestamp('created_at').defaultNow(),
-    updatedAt: timestamp('updated_at').defaultNow(),
-});
+export const comments = pgTable(
+    "comments",
+    {
+        commentId: serial("comment_id").primaryKey(),
+        authId: text("auth_Id")
+            .notNull()
+            .references(() => users.authId, {
+                onDelete: "cascade",
+            }),
+        postId: serial("post_id")
+            .notNull()
+            .references(() => posts.postId, {
+                onDelete: "cascade",
+            }),
+        content: text("content").notNull(),
+        votes: integer("votes").notNull().default(0),
+        createdAt,
+        updatedAt,
+    },
+    (table) => [index("user_comment_idx").on(table.authId)]
+);
 
 // Relations
 export const universityRelations = relations(universities, ({ many }) => ({
@@ -99,12 +156,13 @@ export const reviewRelations = relations(reviews, ({ one }) => ({
 // Users insert schema
 export const insertUserSchema = createInsertSchema(users).omit({
     id: true,
+    createdAt: true,
 });
 
 // University insert schema
 export const insertUniversitySchema = createInsertSchema(universities).omit({
     universityId: true,
-    createdAt: true
+    createdAt: true,
 });
 
 // Review insert schema
@@ -112,14 +170,14 @@ export const insertReviewSchema = createInsertSchema(reviews).omit({
     reviewId: true,
     votes: true,
     createdAt: true,
-    updatedAt: true
+    updatedAt: true,
 });
 
 // Post insert schema
 export const insertPostSchema = createInsertSchema(posts).omit({
     postId: true,
     createdAt: true,
-    updatedAt: true
+    updatedAt: true,
 });
 
 // Comment insert schema
@@ -127,7 +185,7 @@ export const insertCommentSchema = createInsertSchema(comments).omit({
     commentId: true,
     votes: true,
     createdAt: true,
-    updatedAt: true
+    updatedAt: true,
 });
 
 // Types
