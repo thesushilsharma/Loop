f906b282757c98d7516ff089a016a9c1f5a7bfd7
Sushil <ss362@uowmail.edu.au>
2025-02-22 01:21:10 +0400

    fixing some types

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -11,7 +11,6 @@ import {
 } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import { createInsertSchema } from "drizzle-zod";
-import { z } from "zod";
 
 const createdAt = timestamp("created_at").notNull().defaultNow();
 const updatedAt = timestamp("updated_at")
@@ -188,18 +187,3 @@ export const insertCommentSchema = createInsertSchema(comments).omit({
     updatedAt: true,
 });
 
-// Types
-export type University = typeof universities.$inferSelect;
-export type NewUniversity = typeof universities.$inferInsert;
-export type Review = typeof reviews.$inferSelect;
-export type NewReview = typeof reviews.$inferInsert;
-export type Post = typeof posts.$inferSelect;
-export type NewPost = typeof posts.$inferInsert;
-export type Comment = typeof comments.$inferSelect;
-export type NewComment = typeof comments.$inferInsert;
-
-export type InsertUser = z.infer<typeof insertUserSchema>;
-export type InsertUniversity = z.infer<typeof insertUniversitySchema>;
-export type InsertReview = z.infer<typeof insertReviewSchema>;
-export type InsertComment = z.infer<typeof insertCommentSchema>;
-export type InsertPost = z.infer<typeof insertPostSchema>;
--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,14 +1,19 @@
+import { comments, insertCommentSchema, insertPostSchema, insertReviewSchema, insertUniversitySchema, insertUserSchema, posts, reviews, universities } from "@/drizzle/schema";
 import { z } from "zod"
-import { insertUniversitySchema, locationSchema, reviewSchema } from "../schema/uni-validations"
-import { insertCommentSchema, insertPostSchema } from "../schema/post-validations"
 
 
-// University Types
-export type Location = z.infer<typeof locationSchema>
-export type University = z.infer<typeof insertUniversitySchema>
-export type Review = z.infer<typeof reviewSchema>
+// Types
+export type University = typeof universities.$inferSelect;
+export type NewUniversity = typeof universities.$inferInsert;
+export type Review = typeof reviews.$inferSelect;
+export type NewReview = typeof reviews.$inferInsert;
+export type Post = typeof posts.$inferSelect;
+export type NewPost = typeof posts.$inferInsert;
+export type Comment = typeof comments.$inferSelect;
+export type NewComment = typeof comments.$inferInsert;
 
-
-// Post Types
-export type Post = z.infer<typeof insertPostSchema>
-export type Comment = z.infer<typeof insertCommentSchema>
+export type InsertUser = z.infer<typeof insertUserSchema>;
+export type InsertUniversity = z.infer<typeof insertUniversitySchema>;
+export type InsertReview = z.infer<typeof insertReviewSchema>;
+export type InsertComment = z.infer<typeof insertCommentSchema>;
+export type InsertPost = z.infer<typeof insertPostSchema>;
