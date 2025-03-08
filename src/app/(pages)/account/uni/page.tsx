6f48ef5be2f84ce8c5dccd160959e21325dc7a1e
Sushil <ss362@uowmail.edu.au>
2025-03-08 04:17:59 +0400

    adding uni page

--- src/app/(pages)/account/uni/page.tsx
+++ src/app/(pages)/account/uni/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { SearchForm } from "@/components/search-form";
+import { Suspense } from "react";
+import SearchFormSkeleton from "./loading";
 
-export default function Universities() {
+export default async function Universities() {
   return (
-    <div>page</div>
-  )
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold text-primary mb-6 text-center">
+        All Universities
+      </h1>
+
+      <div className="flex justify-center mb-6">
+        <Suspense fallback={<SearchFormSkeleton />}>
+          <SearchForm />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
