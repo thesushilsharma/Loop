7722744f49855519dff2319338f5f6a49c10397a
Sushil <ss362@uowmail.edu.au>
2025-03-18 00:28:48 +0400

    adding uni reviews component

---
+++ src/components/uni-reviews.tsx
@@ -0,0 +1,7 @@
+export async function UniReviews({ universityId }: { universityId: string }) {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-6">Reviews</h2>
+    </div>
+  );
+}
