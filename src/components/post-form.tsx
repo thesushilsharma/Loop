6ca64b049d64cd6fb36904cc58ebe5a0f3f3eb7d
Sushil <ss362@uowmail.edu.au>
2025-03-21 01:41:06 +0400

    adding post form

---
+++ src/components/post-form.tsx
@@ -0,0 +1,7 @@
+export function PostForm({ universityId }: { universityId: string }) {
+  return (
+    <section>
+      <h2 className="text-2xl font-bold mb-6">Post Form</h2>
+    </section>
+  );
+}
