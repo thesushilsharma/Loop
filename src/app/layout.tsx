bdbdb4ffa96eb3226ee3e68c3243343af3561a2a
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-09-01 01:33:32 +0400

    adding changes for toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "sonner";
+import { Toaster } from "@/components/ui/sonner";
 import { AuthProvider } from "@/context/AuthProvider";
 import QueryProvider from "@/context/QueryProvider";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
@@ -39,7 +39,13 @@ export default function RootLayout({
               </main>
             </NuqsAdapter>
           </QueryProvider>
-          <Toaster />
+          <Toaster
+            toastOptions={{
+              style: {
+                background: 'black',
+              },
+            }}
+          />
         </body>
       </html>
     </AuthProvider>
