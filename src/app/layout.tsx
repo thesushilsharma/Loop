4c618a7475e18f3699f44cab498114d40f86d423
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-09-01 01:38:08 +0400

    updating tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { AuthProvider } from "@/context/AuthProvider";
-import QueryProvider from "@/context/QueryProvider";
+import { QueryProvider } from "@/context/QueryProvider";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const geistSans = Geist({
