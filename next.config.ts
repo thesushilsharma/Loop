35371d52f054d6e1ed80d948f861137acdc6bf32
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-09-01 01:29:41 +0400

    updating next js

--- next.config.ts
+++ next.config.ts
@@ -43,11 +43,6 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true, // Enabling SVG
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;", // Additional security for SVGs
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
